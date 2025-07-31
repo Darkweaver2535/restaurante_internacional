f9ea5eab40315cee6e4f10a91359f51219b5a955
Alvaro Encinas <AlvaroEncinas26@gmail.com>
2025-07-31 09:13:35 -0400

    settings 2V

--- sistema_restaurante/settings.py
+++ sistema_restaurante/settings.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
 
+# Puerto para Render
+PORT = int(os.environ.get('PORT', 8000))
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
